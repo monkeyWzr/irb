6a2b3792c1281c1131a90115c2e91176e275f334
Stan Lo <stan001212@gmail.com>
2024-02-01 21:19:04 +0000

    Add IRB::HelperMethod to support helper methods

---
+++ lib/irb/helper_method.rb
@@ -0,0 +1,26 @@
+require_relative "helper_method/base"
+
+module IRB
+  module HelperMethod
+    @helper_methods = {}
+
+    class << self
+      attr_reader :helper_methods
+
+      def register(name, helper_class)
+        @helper_methods[name] = helper_class
+      end
+
+      def all_helper_methods_info
+        @helper_methods.map do |name, helper_class|
+          { display_name: name, description: helper_class.description, category: helper_class.category }
+        end
+      end
+    end
+
+    # Default helper_methods
+    require_relative "helper_method/context"
+    register(:conf, HelperMethod::Conf)
+    register(:context, HelperMethod::Context)
+  end
+end
